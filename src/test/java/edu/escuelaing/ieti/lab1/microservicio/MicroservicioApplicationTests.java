6fc72bbb7b1f2ac6cf5b8914aced2fab452b2475
JamesTorres99 <69390068+JamesTorres99@users.noreply.github.com>
2021-08-26 02:54:29 -0500

    Inicio parte 2

---
+++ src/test/java/edu/escuelaing/ieti/lab1/microservicio/MicroservicioApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.ieti.lab1.microservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
